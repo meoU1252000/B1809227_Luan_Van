5960424e223f4353925876fcdd91cf77fe692e06
meoU1252000 <ltdat1252000@gmail.com>
2022-11-06 13:27:34 +0700

    fix: fix db

--- database/migrations/2022_08_14_122756_create_order_details_table.php
+++ database/migrations/2022_08_14_122756_create_order_details_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('order_details', function (Blueprint $table) {
             $table->foreignId('order_id')->constrained('order')->onDelete('cascade');
-            $table->foreignId('price_id')->constrained('product_price')->onDelete('cascade');
+            $table->double('product_price');
             $table->integer('product_number');
             $table->timestamps();
         });
