863f3f8f29c0f2a02cda2feda6b75e056d8da5ee
meoU1252000 <ltdat1252000@gmail.com>
2022-11-25 01:09:05 +0700

    fix: fix layout

--- resources/views/Admin/dist/creative/import/details/index.blade.php
+++ resources/views/Admin/dist/creative/import/details/index.blade.php
@@ -85,26 +85,6 @@
                                     </table>
                                 </div>
 
-                                <ul class="pagination pagination-rounded justify-content-end my-2">
-                                    <li class="page-item">
-                                        <a class="page-link" href="javascript: void(0);" aria-label="Previous">
-                                            <span aria-hidden="true">«</span>
-                                            <span class="visually-hidden">Previous</span>
-                                        </a>
-                                    </li>
-                                    <li class="page-item active"><a class="page-link" href="javascript: void(0);">1</a>
-                                    </li>
-                                    <li class="page-item"><a class="page-link" href="javascript: void(0);">2</a></li>
-                                    <li class="page-item"><a class="page-link" href="javascript: void(0);">3</a></li>
-                                    <li class="page-item"><a class="page-link" href="javascript: void(0);">4</a></li>
-                                    <li class="page-item"><a class="page-link" href="javascript: void(0);">5</a></li>
-                                    <li class="page-item">
-                                        <a class="page-link" href="javascript: void(0);" aria-label="Next">
-                                            <span aria-hidden="true">»</span>
-                                            <span class="visually-hidden">Next</span>
-                                        </a>
-                                    </li>
-                                </ul>
                             </div> <!-- end card-body-->
                         </div> <!-- end card-->
                     </div> <!-- end col -->
