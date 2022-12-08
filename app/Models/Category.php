89118e649bd1515047865d7cf2bc1b709bf39ac2
meoU1252000 <ltdat1252000@gmail.com>
2022-12-09 04:47:01 +0700

    test

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -38,7 +38,7 @@ class Category extends Model
     }
 
     public function products() {
-       return $this->hasMany(Product::class,'category_id','id');
+       return $this->hasMany(Product::class,'category_id','id')->wherePivot('product_status',1);
 
     }
 }
